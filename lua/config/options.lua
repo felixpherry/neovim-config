85250da9172b26ec1739a126afb3b27808707356
Felix Pherry <123felixpherry@gmail.com>
2024-05-01 12:56:25 +0700

    first commit

---
+++ lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+vim.g.mapleader = " "
+vim.opt.timeoutlen = 0
+
+vim.opt.mouse = ""
+vim.opt.shellcmdflag = "-ic"
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+vim.opt.undofile = false
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/which-key.nvim",
+  opts = function()
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>f"] = { name = "+file" },
+      ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find File" },
+      ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+      ["<leader>fn"] = { "<cmd>enew<cr>" },
+    })
+  end,
+}
